2e864bd6526605fb84305d506e34222ab26d2dc9
Jeremy Leung <jml230@pitt.edu>
2022-07-13 16:33:51 -0400

    changes to parameters to run faster

--- odld_system.py
+++ odld_system.py
@@ -37,7 +37,7 @@ class ODLDPropagator(WESTPropagator):
         self.sigma = 0.001 ** (0.5)  # friction coefficient
 
         self.A = 2
-        self.B = 30
+        self.B = 5
         self.C = 0.5
         self.x0 = 1
         self.y0 = 1
